e08d55667b849622098bbedc0630b097fd198d2d
chimenesjr <7494087+chimenesjr@users.noreply.github.com>
2021-03-13 06:41:15 -0300

    correct file

--- hadoop/Examples/ExemploIGTI.java
+++ hadoop/Examples/ExemploIGTI.java
@@ -21,25 +21,29 @@ public class ExemploIGTI extends Configured implements Tool
 
     public int run (final String[] args) throws Exception {
       try{ 	             	       	
-            JobConf conf = new JobConf(getConf(), ExemploIGTI.class);
-            conf.setJobName("Calculo Covid19");
+            JobConf conf = new JobConf(getConf(), ExemploIGTI.class);			
+            conf.setJobName("Exemplo IGTI - Media");            
+               
+            final FileSystem fs = FileSystem.get(conf); 
+            Path diretorioEntrada = new Path("Entrada"), diretorioSaida = new Path("Saida");            
 
-            final FileSystem fs = FileSystem.get(conf);
-  
-            Path diretorioEntrada = new Path("PastaEntrada"), diretorioSaida = new Path("PastaSaida");
-            fs.mkdirs(diretorioEntrada);
+            /* Criar um diretorio de entrada no HDFS */
+            if (!fs.exists(diretorioEntrada))
+                fs.mkdirs(diretorioEntrada);
 
-            fs.copyFromLocalFile(new Path("/usr/local/hadoop/Dados/covidData.txt"), diretorioEntrada);
+            /* Adicionar um arquivo para ser processado */
+            fs.copyFromLocalFile(new Path("/usr/local/hadoop/Dados/arquivoBigData.txt"), diretorioEntrada);             
 
-            FileInputFormat.setInputPaths(conf, diretorioEntrada);
+            /* Atribuindo os diretorios de Entrada e Saida para o Job */
+            FileInputFormat.setInputPaths(conf,  diretorioEntrada); 
             FileOutputFormat.setOutputPath(conf, diretorioSaida);
-
-            conf.setOutputKeyClass(Text.class);
-            conf.setOutputValueClass(Text.class);
-
+  
+            conf.setOutputKeyClass(Text.class);     
+            conf.setOutputValueClass(Text.class);   
             conf.setMapperClass(MapIGTI.class);
             conf.setReducerClass(ReduceIGTI.class);
-            JobClient.runJob(conf);            
+            JobClient.runJob(conf);   
+                                          
         }
         catch ( Exception e ) {
             throw e;
@@ -50,66 +54,45 @@ public class ExemploIGTI extends Configured implements Tool
     public static class MapIGTI extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
             
       public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter)  throws IOException {
-           Text txtChave = new Text();
-           Text txtValue = new Text();
-           
-           String[] dadosCovid = value.toString().split(",");
-           String dataEvento = dadosCovid[0];
-           String paisEvento = dadosCovid[2];
-           int novosCasos = Integer.parseInt(dadosCovid[4]);
-           int novosObitos = Integer.parseInt(dadosCovid[6]);
 
-           String strChave = dataEvento;
-           String strValor = paisEvento + "|" + String.valueOf(novosCasos) + "|" + String.valueOf(novosObitos);
-           txtChave.set(strChave);
-           txtValue.set(strValor);
-           output.collect(txtChave, txtValue);            
+            Text txtChave = new Text();
+            Text txtValor = new Text();
+             
+            String codigoCliente = value.toString().substring(58, 61);
+            String qtdeItens = value.toString().substring(76, 84);
 
+            txtChave.set(codigoCliente);
+            txtValor.set(qtdeItens);                       
+            
+            output.collect(txtChave, txtValor);            
+
+                       
       }        
     }
  
    
     public static class ReduceIGTI extends MapReduceBase implements Reducer<Text, Text, Text, Text> {       
-       public void reduce (Text key, Iterator<Text> values, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {   
-           int maiorCasos = 0, maiorObitos = 0;
-           String paisCasos = "", paisObitos = "", strSaida = "";
-           Text value = new Text();
-           String[] campos = new String[3];
-           while (values.hasNext()) {
-              value = values.next();   
-              campos = value.toString().split("\\|");
-              if (Integer.parseInt(campos[1]) > maiorCasos) {
-                 maiorCasos = Integer.parseInt(campos[1]);
-                 paisCasos = campos[0];
-              }
-              if (Integer.parseInt(campos[2]) > maiorObitos) {
-                  maiorObitos = Integer.parseInt(campos[2]);
-                  paisObitos = campos[0];
-              }
-           }
-           strSaida = "Casos: " + String.valueOf(maiorCasos) + " em " + paisCasos + ".";
-           strSaida += "Obitos: " + String.valueOf(maiorObitos) + " em " + paisObitos + ".";
-
-           value.set(strSaida);
-           output.collect(key, value);          
- 
-      }                
+      
+       public void reduce (Text key, Iterator<Text> values, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {                                                                                 double media = 0.0; 
+            int acumuladorItens = 0, contaVendas = 0;
+            Text value = new Text();
+        
+            while (values.hasNext()) {
+                value = values.next();               
+                contaVendas++;
+                acumuladorItens += Integer.parseInt(value.toString());
+            }            
+            media = acumuladorItens / new Double(contaVendas);
+                          
+            value.set(String.valueOf(media)); 
+            output.collect(key, value);           
+      }            
+    
     }
-
-   public static class MapIGTIMaior extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
-
-      public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter)  throws IOException {
-
-           
-     }
 }
 
-    public static class ReduceIGTIMaior extends MapReduceBase implements Reducer<Text, Text, Text, Text> {   
-       public void reduce (Text key, Iterator<Text> values, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {      
-         
-             
-  }
-}
 
-}
+
+
+
 
