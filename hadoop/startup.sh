5bd8158f615479f66bc6cab847b6245af45e9657
chimenesjr <chimenesjr@gmail.com>
2021-03-18 11:50:31 +0000

    to help

--- hadoop/startup.sh
+++ hadoop/startup.sh
@@ -60,10 +60,14 @@ docker exec hadoop /usr/local/hadoop/bin/hdfs dfs -cat /user/root/Saida/part-000
 
 # test inside
 # docker exec hadoop mkdir /usr/local/hadoop/examples/ExemploIGTI2
-# docker cp /usr/local/posdc/hadoop/ExemploIGTI2/. hadoop:/usr/local/hadoop/examples/ExemploIGTI2
 # docker exec hadoop ls -l /usr/local/hadoop/examples/ExemploIGTI2
+
+# docker cp /usr/local/posdc/hadoop/ExemploIGTI2/. hadoop:/usr/local/hadoop/examples/ExemploIGTI2
 # docker exec hadoop ant -f /usr/local/hadoop/examples/ExemploIGTI2/build_ExemploIGTI.xml makejar
 # docker exec hadoop ./usr/local/hadoop/bin/hadoop jar /usr/local/hadoop-2.7.1/examples/ExemploIGTI2/ExemploIGTI.jar IGTI.ExemploIGTI
+
+#docker exec hadoop /usr/local/hadoop/bin/hdfs dfs -ls /user/root
+#docker exec hadoop /usr/local/hadoop/bin/hdfs dfs -cat /user/root/Saida73730396-d886-4c38-96f7-9a940bd732af/part-00000
 
 
 # install docker-compose
